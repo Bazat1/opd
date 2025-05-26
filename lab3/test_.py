a2c546c6ac251e6cbe8dfdb1950eba6fad42bb74
Bazat1 <azintrop@mail.ru>
2025-05-26 16:43:23 +0600

    Update test_.py

--- lab3/test_.py
+++ lab3/test_.py
@@ -9,15 +9,15 @@ class TestCurrencyConverter(unittest.TestCase):
         self.app.testing = True
 
     def test_usd_to_rub_conversion(self):
-        """Тестируем конвертацию USD в RUB"""
+        # Тест конвертации USD в RUB
         self.assertAlmostEqual(100 * RATES['USD']['RUB'], 7550.0, places=2)
 
     def test_eur_to_usd_conversion(self):
-        """Тестируем конвертацию EUR в USD"""
+        #Тест конвертации EUR в USD
         self.assertAlmostEqual(50 * RATES['EUR']['USD'], 54.5, places=2)
 
     def test_invalid_currency(self):
-        """Тестируем обработку несуществующей валюты"""
+        #Тест обработки несуществующей валюты
         with self.assertRaises(KeyError):
             invalid_rate = RATES['BTC']['USD']
 
